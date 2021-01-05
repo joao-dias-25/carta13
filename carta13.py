add3c5ba3630c1692a99b85d5717978511be8af2
joao-dias-25 <joaopbdias@gmail.com>
2021-01-06 00:37:55 +0100

    update pic

--- carta13.py
+++ carta13.py
@@ -8,8 +8,11 @@ page_bg_img = '''
 .css-1aumxhk {
 background-image: url("https://www.lavanguardia.com/r/GODO/LV/p3/Portada/2016/12/12/Recortada/img_aaguilarm_20161212-192817_imagenes_lv_otras_fuentes_tarot_de_marseille_major13_death-kXHE--656x1271@LaVanguardia-Web.jpg");
 color: white;
-background-size: cover;
-background-size: 350px 700px
+background-color: #cccccc;
+background-repeat: repeat-x;
+background-attachment: fixed;
+background-position: 50px 0px;
+background-size: 130px 260px;
 }
 </style>
 '''
