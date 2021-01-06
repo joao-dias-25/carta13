b41c52f7f68406693410890c28b244fa0d1f90e6
joao-dias-25 <joaopbdias@gmail.com>
2021-01-06 17:17:27 +0100

    more graphs

--- portugal.py
+++ portugal.py
@@ -14,17 +14,21 @@ def app():
 
     df_dr = pd.read_csv("data/Portugal/pordata.csv", skiprows=7, nrows=61, usecols=range(0, 17))
 
-    '''dfs = {"populacao" : df_pop, "obitos": df_dr}
-    fig = go.Figure()
-    for i in dfs:
-        fig = fig.add_trace(go.Scatter(x = dfs[i]["Unnamed: 0"],
-                                       y = dfs[i]['Total'],
-                                       name = i))
-    st.plotly_chart(fig)'''
+    dfd = pd.read_csv("data/Portugal/Dados_SICO_2021-01-06.csv")
+    dfd = pd.melt(dfd, id_vars=["Data"])
+    dfd = dfd.dropna()
+    dfd.Data = dfd.Data.replace(regex={'Jan': '1', 'Fev': '2', 'Mar': '3', 'Abr': '4', 'Mai': '5', 'Jun': '6',
+                                       'Jul': '7', 'Ago': '8', 'Set': '9', 'Out': '10', 'Nov': '11', 'Dez': '12'})
+    dfd['date'] = dfd["Data"] + '-' + dfd["variable"].astype(str)
+    dfd.date = pd.to_datetime(dfd.date, format="%m-%d-%Y")
+    figo = px.line(dfd, x=dfd.date, y="value",title="Mortalidade diária em Portugal (números oficiais)")
+    figo.update_yaxes(title_text='Óbitos')
+    st.plotly_chart(figo)
 
     fig2 = px.line(df_dr, x="Unnamed: 0", y=['Total', 'Menos de 01',
                                              '01-04', '05-09', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69',
-                                             '70-79', '80-89', '90-99', '100 ou mais'])
+                                             '70-79', '80-89', '90-99', '100 ou mais'],
+                   title="Mortalidade em Portugal desde 1960 (números oficiais)")
 
     fig2.update_yaxes(title_text='Óbitos')
 
