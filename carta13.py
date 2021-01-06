f9451a6db4d81ea71be7476e80736bd92f37580e
joao-dias-25 <joaopbdias@gmail.com>
2021-01-06 15:11:45 +0100

    update locale

--- carta13.py
+++ carta13.py
@@ -24,7 +24,8 @@ background-size: 130px 260px;
 
 </style>
 '''
-locale.setlocale(locale.LC_ALL, "ger_de")
+
+locale.setlocale(locale.LC_ALL, 'en_AG.utf8')
 
 st.sidebar.markdown(page_bg_img, unsafe_allow_html=True)
 
