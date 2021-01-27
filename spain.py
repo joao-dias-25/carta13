a409209c61531a071a847903efbf575357d21867
joao-dias-25 <joaopbdias@gmail.com>
2021-01-27 17:28:42 +0100

    update spain

--- spain.py
+++ spain.py
@@ -12,4 +12,13 @@ def app():
     st.sidebar.write('Source')
     st.sidebar.write('https://www.epdata.es/')
 
-    status = st.radio("Information: ", ('general', 'diagnóstico','Población'))
+    status = st.radio("Information: ", ('General', 'diagnóstico','Población'))
+
+    if (status == 'General'):
+        st.markdown("""
+                    <iframe id='ep-chart-148d86e3-cf82-4b83-bedb-ce12f71c3510' src='https://www.epdata.es/embed/148d86e3-cf82-4b83-bedb-ce12f71c3510/450' style='width: 100%; min-height: 450px; overflow: hidden;' frameborder='0' scrolling='no' height='450'></iframe>
+                    """, unsafe_allow_html=True)
+
+        st.markdown("""
+            <iframe id='ep-chart-a5804d18-8359-43f5-b6e9-05b93e037452' src='https://www.epdata.es/embed/a5804d18-8359-43f5-b6e9-05b93e037452/450' style='width: 100%; min-height: 450px; overflow: hidden;' frameborder='0' scrolling='no' height='450'></iframe>
+            """, unsafe_allow_html=True)
