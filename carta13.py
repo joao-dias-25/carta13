def7cc96bda25a84343ee53484218779d87471a6
joao-dias-25 <joaopbdias@gmail.com>
2021-03-31 17:02:25 +0200

    skull

--- carta13.py
+++ carta13.py
@@ -9,32 +9,9 @@ import userschat
 
 
 st.set_page_config(layout="wide",
-                   page_icon="http://clipartsign.com/upload/2016/06/19/rip-headstone-graveyard-clipart-rip-clipart-graveicon-tv.jpg",
+                   page_icon=":skull:",
                    page_title="Carta 13")
 
-page_bg_img = '''
-<style>
-  body {
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-  }
-
-.css-1aumxhk {
-background-image: url("https://www.lavanguardia.com/r/GODO/LV/p3/Portada/2016/12/12/Recortada/img_aaguilarm_20161212-192817_imagenes_lv_otras_fuentes_tarot_de_marseille_major13_death-kXHE--656x1271@LaVanguardia-Web.jpg");
-color: black;
-background-color: wheat;
-background-repeat: repeat-x;
-background-attachment: fixed;
-background-position: px 0px;
-background-size: 90px 180px;
-}
-
-
-</style>
-'''
-
-
-st.sidebar.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.sidebar.markdown("""<font color='white' size=7  >Carta 13</font>""", unsafe_allow_html=True)
 
